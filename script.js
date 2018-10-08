217a6c5aa55ab951f7727d5940730a7d38c11b54
Aymeric Neveu <emploi.neveu@gmail.com>
2018-10-08 14:03:14 +0200

    exercice7JS

---
+++ script.js
@@ -0,0 +1,6 @@
+function clic() {
+  var shoeSize = document.getElementById("shoeSize").value;
+  var yearOfBirth = document.getElementById("yearOfBirth").value;
+  var result = ((((shoeSize * 2) + 5) * 50) - yearOfBirth + 1766);
+  alert(result);
+}
